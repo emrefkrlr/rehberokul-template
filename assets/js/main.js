63830f2be04c63de3fd717fd276ca58d36a1c7f1
macbookpro <emrefikirlier@gmail.com>
2020-11-07 12:53:15 +0300

    index html done

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,5 +33,18 @@ $(document).ready(function() {
       document.getElementById('')
       
    }
+
+   var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      slidesPerColumn: 2,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+    
 });
+
+
  
