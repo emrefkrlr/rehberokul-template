6c9a039a644fbdca18062060c987a3c80bedc30a
macbookpro <emrefikirlier@gmail.com>
2020-11-15 21:58:59 +0300

    swipper slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,45 +5,7 @@ $(document).ready(function() {
       $(this).toggleClass("is-active");
    });
 
-   var imgNum = 3;
 
-   function right(){
-      if (imgNum != 12) {
-         imgNum +=1;
-         changeImg(imgNum, -1);
-      }
-      else {
-         imgNum = 1;
-         changeImg(imgNum, +2);
-      }
-   }
-
-   function left() {
-      if (imgNum !=1){
-         imgNum -=1;
-         changeImg(imgNum, +1);
-      }
-      else{
-         imgNum = 12;
-         changeImg(imgNum,-2);
-      }
-   }
-
-   function changeImg(value, count) {
-      document.getElementById('')
-      
-   }
-
-   var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 3,
-      slidesPerColumn: 2,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
-    
 });
 
 
